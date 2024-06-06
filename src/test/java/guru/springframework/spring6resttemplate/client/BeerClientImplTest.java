6ca3bba4b89eb1d372a5ad01bbdb452b24dd7094
Wefers, Markus <Markus.Wefers@adesso.de>
2024-06-06 09:06:51 +0200

    1-setup-client - adding client classes

---
+++ src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
@@ -0,0 +1,12 @@
+package guru.springframework.spring6resttemplate.client;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BeerClientImplTest {
+
+    @Test
+    void listBeers() {
+    }
+}
